3198639c25991374bf63a8a783c8d09792fb8b59
Hgm <marekarhim07anshu@gmail.com>
2023-12-16 23:42:01 +0530

    Assign

---
+++ book-app/color_change/index.js
@@ -0,0 +1,23 @@
+function su1() {
+    document.getElementById("bodi1").style.backgroundImage = "linear-gradient(to bottom, orange, white, green)";
+    document.getElementById("r1").innerHTML = "Indian Flag";
+    document.body.style.backgroundImage="linear-gradient(to bottom,  orange, white, green)";
+}
+
+function su2() {
+    document.getElementById("bodi1").style.backgroundImage = "linear-gradient(to bottom, orange, red)";
+    document.getElementById("r1").innerHTML = "Fire";
+    document.body.style.backgroundImage="linear-gradient(to bottom, orange, red)";
+}
+
+function su3() {
+    document.getElementById("bodi1").style.backgroundImage = "linear-gradient(to bottom, aqua, darkblue)";
+    document.getElementById("r1").innerHTML = "Water";
+    document.body.style.backgroundImage="linear-gradient(to bottom, aqua, darkblue)";
+}
+
+function su4() {
+    document.getElementById("bodi1").style.backgroundImage = "linear-gradient(to bottom, white, gray)";
+    document.getElementById("r1").innerHTML = "Clouds";
+    document.body.style.backgroundImage="linear-gradient(to bottom, white, gray)";
+}
